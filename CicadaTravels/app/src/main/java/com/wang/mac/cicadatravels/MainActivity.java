ea36de73e8737bb505b129749c36dee458c59ab6
WoDeYan <12998899243@qq.com>
2016-07-11 19:56:51 +0800

    创建工程

---
+++ CicadaTravels/app/src/main/java/com/wang/mac/cicadatravels/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wang.mac.cicadatravels;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
